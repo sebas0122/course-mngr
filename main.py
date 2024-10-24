8c9c2174b923594bc1cb061e6e1ff18edbfa6877
Nelson Parra <nelsonfernandopg@gmail.com>
2024-10-24 11:15:31 -0500

    simple button app

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 import sys, os
-from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QApplication, QLabel, QPushButton, QMainWindow
-from PySide6 import QtCore
-                                                     
+from PySide6.QtCore import Qt, QSize
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
+
 class MainWindow(QMainWindow):
     def __init__(self):
-        super(MainWindow, self).__init__()
-        label = QLabel("Hello World", alignment=Qt.Alignment.AlignCenter)
-        button = QPushButton(self.tr("Rock and Roll"), self)
-        button.setShortcut(self.tr("Alt+F4"))
-        label.show()
+        super().__init__()
 
+        self.button_is_checked = True
+
+        self.setWindowTitle("My App")
+
+        button = QPushButton("Press Me!")
+        button.setCheckable(True)
+        button.clicked.connect(self.the_button_was_toggled)
+        button.setChecked(self.button_is_checked)
+
+        self.setCentralWidget(button)
+
+    def the_button_was_toggled(self, checked):
+        self.button_is_checked = checked
+
+        print(self.button_is_checked)
+
+                                                     
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    translator = QtCore.QTranslator(app)
-    translator.load('i18n/tr_de', os.path.dirname(__file__))
-    app.installTranslator(translator)
     window = MainWindow()
     window.show()
-    sys.exit(app.exec())
+    app.exec()
