9cfd6f81641fa140eaa1098c264b60280b20ea72
Nelson Parra <nelsonfernandopg@gmail.com>
2025-01-11 11:40:24 -0500

    advancements in data exctraction from exel

--- excel_analysis.py
+++ excel_analysis.py
@@ -10,7 +10,7 @@ def read_excel_file(file_path):
 
 def getClassSchedule(days_hours_string):
     days_dict = {"L": "Lunes", "M": "Martes", "W": "Miércoles", "J": "Jueves", "V": "Viernes", "S": "Sábado", "D": "Domingo"}
-    days_hours_list = days_hours_string.split(" ")
+    days_hours_list = days_hours_string.split("|")
     days = []
     start_hours = []
     class_duration = []
@@ -28,26 +28,68 @@ def getClassSchedule(days_hours_string):
 
     return days, start_hours, class_duration
 
+def getHoursLong(days_hours_string):
+    if "|" in days_hours_string:
+        _, _, x = getClassSchedule(days_hours_string)
+        return int(sum(x))
+    else:
+        print(days_hours_string, days_hours_string[2:])
+        x = days_hours_string[1:].split("-")
+        return int(x[1]) - int(x[0])
+
 def getCleanData(dataframe):
+
+    courses = []
+    for_db = []
+    
     df_fac   = dataframe["FAC"]
     df_dep   = dataframe["DEP"]
+    df_ide   = dataframe["IDE"]
     df_mat   = dataframe["MAT"]
     df_group = dataframe["GRUPO"]
     df_aula  = dataframe["AULA"]
     df_hora  = dataframe["HORARIO"]
+    df_prof_id = dataframe["CÉDULA"]
+
+    df_carrera = dataframe["VERSIÓN"]
+    nivel = ''
+    electiva = False
     
     for column_name, df_materia in dataframe.items():
         if column_name == "MATERIA":
             for i in range(len(df_materia)):
+                if df_carrera[i] == "INGENIERÍA DE TELECOMUNICACIONES PRESENCIAL":
+                    break
                 if type(df_fac[i]) == str:
-                    print(df_fac[i])
+                    if df_fac[i][-1] in ['1', '2', '3', '4', '5', '6', '7', '8', '9']:
+                        nivel = df_fac[i][-1]
+                    else:
+                        nivel = 0
+                        electiva = True
+
                 if type(df_materia[i]) == str:
                     if type(df_fac[i+1]) == float:
-                        print(df_fac[i], int(df_dep[i]), df_mat[i], int(df_group[i]), df_materia[i], df_aula[i], df_hora[i])
-                        print(df_fac[i], int(df_dep[i]), df_mat[i], int(df_group[i]), df_materia[i], df_aula[i+1], df_hora[i+1])
+                        courses.append([
+                            str(df_fac[i]) + str(int(df_dep[i])) + str(df_mat[i]),
+                            int(df_group[i]), df_materia[i], df_aula[i], df_aula[i+1], df_hora[i], df_hora[i+1]
+                            ])
+                        for_db.append([df_materia[i], int(df_fac[i]), int(df_dep[i]), df_ide[i],
+                                               int(df_mat[i]), 'T-P', int(nivel), getHoursLong(df_hora[i]),
+                                               getHoursLong(df_hora[i+1]), 0, electiva, False,
+                                               f'{df_hora[i]} {df_hora[i+1]}', df_prof_id[i], df_prof_id[i+1], df_aula[i], df_aula[i+1]])
                     else:
-                        print(df_fac[i], int(df_dep[i]), df_mat[i], int(df_group[i]), df_materia[i], df_aula[i], df_hora[i])
+                        courses.append([
+                            str(df_fac[i]) + str(int(df_dep[i])) + str(df_mat[i]),
+                            int(df_group[i]), df_materia[i], df_aula[i], df_hora[i]
+                            ])
+                        for_db.append([df_materia[i], int(df_fac[i]), int(df_dep[i]), df_ide[i],
+                                               int(df_mat[i]), 'T', int(nivel), getHoursLong(df_hora[i]),
+                                               0, 0, electiva, False, df_hora[i], df_prof_id[i],
+                                               df_aula[i], 'NO'])
+    print(for_db)
 
+# days, _, duration = getClassSchedule("L8-10|M14-16")
+# print(days, duration)
 file_path = "data/A_PROGRAMACION.xlsx"
 dataframe = read_excel_file(file_path)
 getCleanData(dataframe)
