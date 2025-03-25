04b87d29bb2f639fbfab964606792695dc8fe270
Nelson Parra <nelsonfernandopg@gmail.com>
2025-03-25 16:21:27 -0500

    new interface with movable blocks

--- main.py
+++ main.py
@@ -1,222 +1,98 @@
-import tkinter
+from tkinter import *
 
-# The factory function
-def dnd_start(source, event):
-    h = DndHandler(source, event)
-    if h.root:
-        return h
-    else:
-        return None
+geometry_x = 800
+geometry_y = 600
+padding_factor = 0.001
+unit_cell_factor_x = 0.008
+unit_cell_factor_y = 0.008
 
+# Monday, Tuesday, Wednesday, Thursday, Friday, Saturday
+# class_hours, hours must add 16
+classes = [
+    ["class1_2", "class2_2", "class3_12"],
+    ["class1_1", "class2_1", "class3_14"],
+    ["class1_3", "class2_3", "class3_13"],
+    ["class1_4", "class2_4", "class3_8"],
+    ["class1_5", "class2_5", "class3_6"],
+    ["class1_6", "class2_6", "class3_4"],
+    
+]
 
-# The class that does the work
-class DndHandler:
-
-    root = None
-
-    def __init__(self, source, event):
-        if event.num > 5:
-            return
-        root = event.widget._root()
-        try:
-            root.__dnd
-            return # Don't start recursive dnd
-        except AttributeError:
-            root.__dnd = self
-            self.root = root
-        self.source = source
-        self.target = None
-        self.initial_button = button = event.num
-        self.initial_widget = widget = event.widget
-        self.release_pattern = "<B%d-ButtonRelease-%d>" % (button, button)
-        self.save_cursor = widget['cursor'] or ""
-        widget.bind(self.release_pattern, self.on_release)
-        widget.bind("<Motion>", self.on_motion)
-        widget['cursor'] = "hand2"
-
-    def __del__(self):
-        root = self.root
-        self.root = None
-        if root:
-            try:
-                del root.__dnd
-            except AttributeError:
-                pass
-
-    def on_motion(self, event):
-        x, y = event.x_root, event.y_root
-        target_widget = self.initial_widget.winfo_containing(x, y)
-        source = self.source
-        new_target = None
-        while target_widget:
-            try:
-                attr = target_widget.dnd_accept
-            except AttributeError:
-                pass
-            else:
-                new_target = attr(source, event)
-                if new_target:
-                    break
-            target_widget = target_widget.master
-        old_target = self.target
-        if old_target is new_target:
-            if old_target:
-                old_target.dnd_motion(source, event)
-        else:
-            if old_target:
-                self.target = None
-                old_target.dnd_leave(source, event)
-            if new_target:
-                new_target.dnd_enter(source, event)
-                self.target = new_target
+class dnd_label:
+    def __init__(self, text, bg_color, w, h, posx, posy):
+        self.label = Label(window, text=text, bg=bg_color, width=w, height=h)
+        self.label.place(x=posx, y=posy)
+        self.label.bind("<Button-1>", self.on_press)
+        self.label.bind("<B1-Motion>", self.on_drag)
+        self.label.bind("<ButtonRelease-1>", self.on_release)
+    
+    def on_press(self, event):
+        self.x = event.x
+        self.y = event.y
+    
+    def on_drag(self, event):
+        x = self.label.winfo_x() - self.x + event.x
+        y = self.label.winfo_y() - self.y + event.y
+        self.label.place(x=x, y=y)
 
     def on_release(self, event):
-        print("on_release: x=%d, y=%d" % (event.x, event.y))
-        print("on_release: x=%d, y=%d" % (event.x_root, event.y_root))
-        print(self.source.get_name())
-        self.finish(event, 1)
+        print("on_release: x=%d, y=%d" % (self.label.winfo_x(), self.label.winfo_y()))
 
-    def cancel(self, event=None):
-        self.finish(event, 0)
+        # Check if it is moved out of the window
+        if self.label.winfo_x() < 0 and self.label.winfo_y() < 0:
+            self.label.place(x=0, y=0)
+        elif self.label.winfo_x() < 0:
+            self.label.place(x=0, y=self.label.winfo_y())
+        elif self.label.winfo_y() < 0:
+            self.label.place(x=self.label.winfo_x(), y=0)
 
-    def finish(self, event, commit=0):
-        target = self.target
-        source = self.source
-        widget = self.initial_widget
-        root = self.root
-        try:
-            del root.__dnd
-            self.initial_widget.unbind(self.release_pattern)
-            self.initial_widget.unbind("<Motion>")
-            widget['cursor'] = self.save_cursor
-            self.target = self.source = self.initial_widget = self.root = None
-            if target:
-                if commit:
-                    target.dnd_commit(source, event)
-                else:
-                    target.dnd_leave(source, event)
-        finally:
-            source.dnd_end(target, event)
+        # Check if it is moved out of the window
+        if self.label.winfo_x() + self.label.winfo_width() > geometry_x and self.label.winfo_y() + self.label.winfo_height() > geometry_y:
+            self.label.place(x=geometry_x - self.label.winfo_width(), y=geometry_y - self.label.winfo_height())
+        elif self.label.winfo_x() + self.label.winfo_width() > geometry_x:
+            self.label.place(x=geometry_x - self.label.winfo_width(), y=self.label.winfo_y())
+        elif self.label.winfo_y() + self.label.winfo_height() > geometry_y:
+            self.label.place(x=self.label.winfo_x(), y=geometry_y - self.label.winfo_height())
+
+def insert_ver_divider(window, x, y, w, h):
+    label = Label(window, width=w, height=h, bg="black")
+    label.place(x=x, y=y)
+    label.pack()
+
+def insert_hor_divider(canvas, x, y, w, h):
+    canvas.create_line(x, y, w, h, fill="black")
+
+window = Tk()
+
+window.geometry(f"{geometry_x}x{geometry_y}")
+but = Button(command=window.quit, text="Quit")
+but.place(x=0, y=0)
 
 
+# Draw schedule's hours
+w = int(window.winfo_width() * unit_cell_factor_x)
+h = int(window.winfo_height() * unit_cell_factor_y)
+print(w,h)
 
-# ----------------------------------------------------------------------
-# The rest is here for testing and demonstration purposes only!
+# Add labels for hours
+for hour in range(6, 22):
+    label = Label(window, text=f"{hour}:00", bg="white", width=10, height=1, borderwidth=2, relief="raised")
+    label.place(x=0, y=(hour-4)*25)
 
-class Icon:
+# Add labels for days
+days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+for day in range(6):
+    label = Label(window, text=days[day], bg="white", width=10, height=1)
+    label.place(x=(day+1)*100, y=0)
 
-    def __init__(self, name):
-        self.name = name
-        self.canvas = self.label = self.id = None
+# Add classes
+i = 0
+for day in classes:
+    j=0
+    for cl in day:
+        temp = cl.split("_")
+        dnd_label(temp[0], "red", 10, int(temp[1]), (i+1)*100, (j+2)*25)
+        j+=int(temp[1])
+    i+=1
 
-    def attach(self, canvas, x=10, y=10):
-        if canvas is self.canvas:
-            self.canvas.coords(self.id, x, y)
-            return
-        if self.canvas:
-            self.detach()
-        if not canvas:
-            return
-        label = tkinter.Label(canvas, text=self.name,
-                              borderwidth=2, relief="raised")
-        id = canvas.create_window(x, y, window=label, anchor="nw")
-        self.canvas = canvas
-        self.label = label
-        self.id = id
-        label.bind("<ButtonPress>", self.press)
-
-    def detach(self):
-        canvas = self.canvas
-        if not canvas:
-            return
-        id = self.id
-        label = self.label
-        self.canvas = self.label = self.id = None
-        canvas.delete(id)
-        label.destroy()
-
-    def press(self, event):
-        if dnd_start(self, event):
-            # where the pointer is relative to the label widget:
-            self.x_off = event.x
-            self.y_off = event.y
-            # where the widget is relative to the canvas:
-            self.x_orig, self.y_orig = self.canvas.coords(self.id)
-
-    def move(self, event):
-        x, y = self.where(self.canvas, event)
-        self.canvas.coords(self.id, x, y)
-
-    def putback(self):
-        self.canvas.coords(self.id, self.x_orig, self.y_orig)
-
-    def where(self, canvas, event):
-        # where the corner of the canvas is relative to the screen:
-        x_org = canvas.winfo_rootx()
-        y_org = canvas.winfo_rooty()
-        # where the pointer is relative to the canvas widget:
-        x = event.x_root - x_org
-        y = event.y_root - y_org
-        # compensate for initial pointer offset
-        return x - self.x_off, y - self.y_off
-
-    def dnd_end(self, target, event):
-        pass
-
-    def get_name(self):
-        return self.name
-
-class Tester:
-
-    def __init__(self, root):
-        self.top = tkinter.Toplevel(root)
-        self.canvas = tkinter.Canvas(self.top, width=100, height=100)
-        self.canvas.pack(fill="both", expand=1)
-        self.canvas.dnd_accept = self.dnd_accept
-
-    def dnd_accept(self, source, event):
-        return self
-
-    def dnd_enter(self, source, event):
-        self.canvas.focus_set() # Show highlight border
-        x, y = source.where(self.canvas, event)
-        x1, y1, x2, y2 = source.canvas.bbox(source.id)
-        dx, dy = x2-x1, y2-y1
-        self.dndid = self.canvas.create_rectangle(x, y, x+dx, y+dy)
-        self.dnd_motion(source, event)
-
-    def dnd_motion(self, source, event):
-        x, y = source.where(self.canvas, event)
-        x1, y1, x2, y2 = self.canvas.bbox(self.dndid)
-        self.canvas.move(self.dndid, x-x1, y-y1)
-
-    def dnd_leave(self, source, event):
-        self.top.focus_set() # Hide highlight border
-        self.canvas.delete(self.dndid)
-        self.dndid = None
-
-    def dnd_commit(self, source, event):
-        self.dnd_leave(source, event)
-        x, y = source.where(self.canvas, event)
-        source.attach(self.canvas, x, y)
-
-def test():
-    root = tkinter.Tk()
-    root.geometry("+1+1")
-    tkinter.Button(command=root.quit, text="Quit").pack()
-    t1 = Tester(root)
-    t1.top.geometry("+1+60")
-    t2 = Tester(root)
-    t2.top.geometry("+120+60")
-    t3 = Tester(root)
-    t3.top.geometry("+240+60")
-    i1 = Icon("ICON1")
-    i2 = Icon("ICON2")
-    i3 = Icon("ICON3")
-    i1.attach(t1.canvas)
-    i2.attach(t1.canvas)
-    i3.attach(t3.canvas)
-    root.mainloop()
-
-if __name__ == '__main__':
-    test()
+window.mainloop()
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,6 @@
-nivel
+for day in classes:
+#     i = 0
+#     for cl in day:
+#         temp = cl.split("_")
+#         dnd_label(temp[0], "red", 10, 1, (day+1)*100, (int(temp[1])))
+#     i+=1
